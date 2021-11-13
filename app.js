af596ddbd2bbdc6b4536cd498c6fb6d1148dc936
Daniel Felipe <felipeguisao18@gmail.com>
2021-11-12 20:25:23 -0500

    docs: add public, routes and views

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-
 const express = require('express')
-const app = express()
-const mysql = require('mysql2')
+const aplicacion = express()
 const bodyParser = require('body-parser')
 const session = require('express-session')
 const flash = require('express-flash')
+const fileUpload = require('express-fileupload')
 
-var pool = mysql.createPool({  
-    connectionLimit: 20,  
-    host: 'localhost',  
-    user: 'root',  
-    password: '317danii07',  
-    database: 'blog_viajes' 
+const rutasMiddleware = require('./routes/middleware')
+const rutasPublicas = require('./routes/publicas')
+const rutasPrivadas = require('./routes/privadas')
+
+aplicacion.use(bodyParser.json())
+aplicacion.use(bodyParser.urlencoded({ extended: true }))
+aplicacion.set("view engine", "ejs")
+aplicacion.use(session({ secret: 'token-muy-secreto', resave: true, saveUninitialized: true }));
+aplicacion.use(flash())
+aplicacion.use(express.static('public'))
+aplicacion.use(fileUpload())
+
+aplicacion.use(rutasMiddleware)
+aplicacion.use(rutasPublicas)
+aplicacion.use(rutasPrivadas)
+
+aplicacion.listen(8080, () => {
+  console.log("Servidor iniciado")
 })
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.set('view engine', 'ejs')
-app.use(session({ secret: 'token-muy-secreto', resave: true, saveUninitialized: true }))
-app.use(flash())
-app.use(express.static('public'))
-
-
-app.get('/', function (peticion, respuesta) {
-    pool.getConnection(function(err, connection) {
-      const consulta = `
-        SELECT
-        titulo, resumen, fecha_hora, pseudonimo, votos
-        FROM publicaciones
-        INNER JOIN autores
-        ON publicaciones.autor_id = autores.id
-        ORDER BY fecha_hora DESC
-        LIMIT 5
-      `
-      connection.query(consulta, function (error, filas, campos) {
-        respuesta.render('index', { publicaciones: filas })
-      })
-      connection.release()
-    })
-})
-
-app.listen(8080, function(){
-    console.log("Servidor iniciado");
-  });
-  
